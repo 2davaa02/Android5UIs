308e242a394aa4c83c4f70e8be383f283a6dc362
Alexandru-Mihai Davalciuc <2davaa02@solent.ac.uk>
2018-03-14 10:57:27 +0000

    Initial commit

---
+++ app/src/main/java/com/example/a2davaa02/android5uis/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.a2davaa02.android5uis;
+
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar1);
+        setSupportActionBar(toolbar);
+        FloatingActionButton fab=new FloatingActionButton(this);
+        fab.setOnClickListener (new View.OnClickListener() {
+
+            public void onClick(View view) {
+                new AlertDialog.Builder(MainActivity.this).setPositiveButton("OK", null).
+                        setMessage("The FloatingActionButton was clicked!").show();
+            }
+        });
+
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        MenuItem item = menu.findItem(R.id.action_search);
+        SearchView sv = (SearchView) MenuItemCompat.getActionView(item);
+        sv.setOnQueryTextListener(new SearchHandler());
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if(item.getItemId()==R.id.save)
+        {
+            new AlertDialog.Builder(MainActivity.this).setPositiveButton("OK", null).
+                    setMessage("Saving..").show();
+            return true;
+        }
+        return false;
+    }
+    class SearchHandler implements SearchView.OnQueryTextListener {
+
+
+        public SearchHandler(){
+
+        }
+
+        public boolean onQueryTextChange(String txt) {
+            // do nothing... (this method runs when the user types a new character)
+            return true;
+        }
+
+        public boolean onQueryTextSubmit(String txt) {
+            // show the search text in an alert dialog
+            new AlertDialog.Builder(MainActivity.this).setPositiveButton("OK", null).
+                    setMessage(txt).show();
+            return true;
+        }
+    }
+
+}
